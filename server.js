bdb24d1d086a680fd852be27102c435d723b8e7a
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-09-07 11:08:05 +0530

    chore: setuo the express server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const indexRouter = require('./routes/indexRouter')
+const newRouter = require('./routes/newRouter')
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+app.use("/", indexRouter)
+app.use("/new", newRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
