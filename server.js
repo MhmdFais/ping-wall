866b2bea0afbbba1b431b499681ec859e858f24d
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-09-07 18:32:19 +0530

    style: home page styling done

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
@@ -14,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter)
 app.use("/new", newRouter)
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
